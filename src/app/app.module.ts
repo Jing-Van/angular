ac2a0baa21ad12182d2f450e6db8403582048fb2
jing <qinmin@hotmail.ca>
2018-07-18 22:57:55 -0700

    Task 3 log in page working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {PreloadAllModules, RouterModule} from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import {ROUTES} from './app.routes';
+import { LoginComponent } from './login/login.component';
+import { HttpModule } from '@angular/http';
+import { Http } from '@angular/http';
+import { AlertComponent } from './_directives';
+import { AuthGuard } from './_guards';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AlertService, AuthenticationService, UserService } from './_services';
+import { RegisterComponent } from './register';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
+    HttpModule,
     RouterModule.forRoot(ROUTES, {
       useHash: true,
       preloadingStrategy: PreloadAllModules
     })
   ],
-  providers: [],
+  providers: [
+      AuthGuard,
+      AlertService,
+      AuthenticationService,
+      UserService,
+      { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+      { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+      // provider used to create fake backend
+      fakeBackendProvider
+        
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import {Routes} from '@angular/router';
 
+import {LoginComponent} from './login/login.component';
+import { AuthGuard } from './_guards';
+import { RegisterComponent } from './register';
+
 export const ROUTES: Routes = [
-  {path: '', redirectTo: 'app', pathMatch: 'full'},
+
+  {path: '', redirectTo: 'app', pathMatch: 'full', canActivate: [AuthGuard]},
   {path: 'app', loadChildren: './layout/layout.module#LayoutModule'}
+  {path: 'login', component: LoginComponent }, 
+  {path: 'register', component: RegisterComponent },
+
 ];
